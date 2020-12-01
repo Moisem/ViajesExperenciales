37db27e17304bf28e38a49937f4864b70d531da5
Moisem <al221910022@gmail.com>
2020-11-30 22:50:07 -0600

    correccion

--- database/migrations/2020_01_01_041946_create_users_table.php
+++ database/migrations/2020_01_01_041946_create_users_table.php
@@ -24,7 +24,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('cargo',['turista','administrador']);
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
